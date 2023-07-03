a8b97348b1ffbfce4283f15706ce957c8e1a4f50
Vasya-556 <vkobrin556@gmail.com>
2023-07-03 22:09:34 +0300

    sixth commit

---
+++ src/components/EditTask.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { updateDoc, doc } from 'firebase/firestore';
+import db from './Firebase';
+
+function EditTask({ taskId, initialName, initialDescription, toggleEdit }) {
+  const [editName, setEditName] = useState(initialName);
+  const [editDescription, setEditDescription] = useState(initialDescription);
+
+  const handleEditNameChange = (event) => {
+    setEditName(event.target.value);
+  };
+
+  const handleEditDescriptionChange = (event) => {
+    setEditDescription(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await updateDoc(doc(db, 'tasks', taskId), {
+        taskName: editName,
+        taskDescription: editDescription
+      });
+      console.log('Document updated successfully');
+
+      // Закриття форми редагування після збереження даних
+      toggleEdit();
+    } catch (error) {
+      console.error('Error updating document:', error);
+    }
+  };
+
+  return (
+    <form className='edit-form' onSubmit={handleSubmit}>
+      <input
+        className='edit-name'
+        type='text'
+        value={editName}
+        onChange={handleEditNameChange}
+        required
+      />
+      <textarea
+        className='edit-description'
+        value={editDescription}
+        onChange={handleEditDescriptionChange}
+        required
+      ></textarea>
+      <button type='submit'>Save</button>
+    </form>
+  );
+}
+
+export default EditTask;
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GrEdit } from 'react-icons/gr';
 import { FaTrash } from 'react-icons/fa';
+import EditTask from './EditTask';
 
 function Task({ id, taskName, taskDescription, deleteTask }) {
-  // const handleDelete = () => {
-  //   deleteTask(id);
-  // };
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleDelete = () => {
+    deleteTask(id);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
 
   return (
     <div className='Task'>
-      <FaTrash className='delete-icon' /*onClick={Delete}*/ />
-      <GrEdit className='edit-icon' /*onClick={Edit}*//>
+      <FaTrash className='delete-icon' onClick={handleDelete} />
+      <GrEdit className='edit-icon' onClick={handleEdit} />
       <h3 className='TaskName'>{taskName}</h3>
       <p className='TaskDescription'>{taskDescription}</p>
+      {isEditing && (
+        <EditTask
+          taskId={id}
+          initialName={taskName}
+          initialDescription={taskDescription}
+          toggleEdit={handleEdit}
+        />
+      )}
     </div>
   );
 }
